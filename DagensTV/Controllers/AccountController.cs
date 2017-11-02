997f5506713298cb0f03affd5c9e323bc8e2028a
Linda Pettersson <pettersson.linda08@gmail.com>
2017-11-02 14:32:03 +0100

    Skapat LoginVM

--- DagensTV/Controllers/AccountController.cs
+++ DagensTV/Controllers/AccountController.cs
@@ -9,7 +9,7 @@ namespace DagensTV.Controllers
     public class AccountController : Controller
     {
         // GET: Account
-        public ActionResult Index()
+        public ActionResult Login()
         {
             return View();
         }
