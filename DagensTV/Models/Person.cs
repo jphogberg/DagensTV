d330ad9deb7612f53d05899bf10907678379de4b
Linda Pettersson <pettersson.linda08@gmail.com>
2017-11-09 13:54:04 +0100

    ändrat i db

--- DagensTV/Models/Person.cs
+++ DagensTV/Models/Person.cs
@@ -14,29 +14,13 @@ namespace DagensTV.Models
     
     public partial class Person
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public Person()
-        {
-            this.Settings1 = new HashSet<Settings>();
-        }
-    
         public int Id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public Nullable<int> RoleId { get; set; }
-        public Nullable<int> SettingsId { get; set; }
-
-        public string RoleName { get; set; }
-
-        public static Person activeUser = new Person();
-        public List<Channel> myChannels = new List<Channel>();
-        public List<bool> mySettings = new List<bool>();
-
+    
         public virtual Role Role { get; set; }
-        public virtual Settings Settings { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Settings> Settings1 { get; set; }
     }
 }
