eac064ea339f2f102f200e9076a8ba9679c72ee8
Patrik Högberg <pahg1601@student.miun.se>
2017-11-09 20:57:57 +0100

    Ger upp tillsvidare med progressbar, går över till annat. :(

--- DagensTV/Scripts/myjscript.js
+++ DagensTV/Scripts/myjscript.js
@@ -6,13 +6,13 @@ function toggleMenu(x) {
         x.className += " responsive";
     } else {
         x.className = "menu";
-    }    
+    }
 }
 
 /* Visa & göm Overlay */
 var modal = document.getElementById('search-overlay');
 
-function showOverlay(show) {       
+function showOverlay(show) {
     modal.display = document.getElementById("overlay-body").innerHTML = show;
     modal.style.display = "block";
 }
@@ -28,17 +28,75 @@ window.onclick = function (event) {
 };
 
 /* Progressbar */
-var startStr = $('.track-time').data('start');
-var endStr = $('.track-time').data('end');
 
-now = moment();
-var startTime = moment(startStr);
-var endTime = moment(endStr);
-var percentage = (now - startTime) / (endTime - startTime) * 100;
-console.log(percentage)
+//$("[class*=track-time]").get(function (index, val) {
 
-$(function () {
-    $(".active-programme #progressbar").progressbar({
-        value: percentage
-    });
-});
+//    var arrValues = [ "76", "83" ];
+//    var startStr = $(val).data('start');
+//    var endStr = $(val).data('end');
+
+//    now = moment();
+//    var startTime = moment(startStr);
+//    var endTime = moment(endStr);
+//    var percentage = (now - startTime) / (endTime - startTime) * 100;
+
+//    arrValues.push(percentage);        
+
+//    console.log(arrValues)
+
+//    $("#progressbar").each(function () {
+//        $(".active-programme #progressbar").progressbar({
+//  .active-programme [class*=track-time]          value: arrValues[0]
+//    })
+
+//    });
+//})
+
+var now, nowMS, startStr, startMS, endStr, endMS, percentage, pbarsAmount, channelsAmount, percentage;
+elements = $(".channel-class .active-programme #progressbar");
+elements.each(function (i, obj) {
+
+    pbarsAmount = document.getElementsByClassName("active-programme");
+    channelsAmount = document.getElementsByClassName("channel-class");
+
+    now = new Date();
+    nowMS = now.getTime();
+    startStr = $(this).data('start');
+    startMS = Date.parse(startStr);
+    endStr = $(this).data('end');
+    endMS = Date.parse(endStr);
+
+    percentage = Math.round(((nowMS - startMS) / (endMS - startMS)) * 100);
+
+    //var perStr = percentage.toString();
+
+
+
+
+    console.log(startStr)
+    $(".active-programme [class*=track-time]").progressbar({
+        value: parseInt(percentage)
+    })
+
+})
+
+//for (var k = 0; k < channelsAmount.length; k++) {    
+
+//    for (var j = 0; j < pbarsAmount.length; j++) {        
+//        $(".active-programme [class*=track-time]").progressbar({
+//            value: parseInt(percentage)
+//        })
+//    }
+
+//}
+
+
+
+
+
+    //$("#progressbar").each(function () {
+    //    $(".active-programme #progressbar").progressbar({
+    //        value: 63
+    //    })
+    //})
+
